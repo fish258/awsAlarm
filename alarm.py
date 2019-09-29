8959e083b6570af7318dc049afcdaf7c2a0b8e5c
fish258 <44438335+fish258@users.noreply.github.com>
2019-09-29 19:12:27 +1300

    Update alarm.py

--- alarm.py
+++ alarm.py
@@ -7,6 +7,8 @@ p1 = os.popen("aws sns create-topic --name my-alarm")
 arn = p1.read().strip()
 p2 = os.popen("ec2metadata --instance-id")
 ec2ID = p2.read().strip()
+email = "1074623886@qq.com"
+os.system("aws sns subscribe --topic-arn %s --protocol email --notification-endpoint %s"%(arn,email))
 os.system("aws cloudwatch put-metric-alarm --alarm-name cpu-mon+%s --alarm-description "Alarm when CPU exceeds 70 percent" --metric-name CPUUtilization --namespace AWS/EC2 --statistic Average --period 300 --threshold 70 --comparison-operator GreaterThanThreshold --dimensions  Name=InstanceId,Value=%s --evaluation-periods 2 --alarm-actions %s --unit Percent"%(ec2ID,ec2ID,arn))
 os.system("aws cloudwatch put-metric-alarm --alarm-name cpu-CreditUsage+%s --alarm-description "Alarm when CPU Credit Usage exceeds 70 percent" --metric-name CPUCreditUsage --namespace AWS/EC2 --statistic Average --period 300 --threshold 70 --comparison-operator GreaterThanThreshold --dimensions  Name=InstanceId,Value=%s --evaluation-periods 2 --alarm-actions %s --unit Percent"%(ec2ID,ec2ID,arn))
 os.system("aws cloudwatch put-metric-alarm --alarm-name lb-mon+%s --alarm-description "Alarm when Latency exceeds 100s" --metric-name Latency --namespace AWS/ELB --statistic Average --period 60 --threshold 100 --comparison-operator GreaterThanThreshold --dimensions Name=InstanceId,Value=%s --evaluation-periods 3 --alarm-actions %s --unit Seconds"%(ec2ID,ec2ID,arn))
