8b384070cc3af4fc3e7b593d4ae58ffdb36a1a6e
fish258 <44438335+fish258@users.noreply.github.com>
2019-09-29 19:16:34 +1300

    Update alarm.py

--- alarm.py
+++ alarm.py
@@ -3,10 +3,10 @@ import os
 os.system("sudo apt-get update")
 os.system("sudo apt-get -y install awscli")
 os.system("aws configure")
-p1 = os.popen("aws sns create-topic --name my-alarm")
-arn = p1.read().strip()
 p2 = os.popen("ec2metadata --instance-id")
 ec2ID = p2.read().strip()
+p1 = os.popen("aws sns create-topic --name alarm_%s"%(p2))
+arn = p1.read().strip()
 email = "1074623886@qq.com"
 os.system("aws sns subscribe --topic-arn %s --protocol email --notification-endpoint %s"%(arn,email))
 os.system("aws cloudwatch put-metric-alarm --alarm-name cpu-mon+%s --alarm-description "Alarm when CPU exceeds 70 percent" --metric-name CPUUtilization --namespace AWS/EC2 --statistic Average --period 300 --threshold 70 --comparison-operator GreaterThanThreshold --dimensions  Name=InstanceId,Value=%s --evaluation-periods 2 --alarm-actions %s --unit Percent"%(ec2ID,ec2ID,arn))
