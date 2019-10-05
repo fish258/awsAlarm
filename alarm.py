7858bcd05ea0661816c254b119b6c82fece4c985
fish258 <44438335+fish258@users.noreply.github.com>
2019-10-06 00:16:20 +1300

    Update alarm.py

--- alarm.py
+++ alarm.py
@@ -8,7 +8,6 @@ ec2ID = p2.read().strip()
 p1 = os.popen("aws sns create-topic --name alarm")
 arn = p1.read().strip()
 region = arn[12:21]
-print(region)
 email = "1074623886@qq.com"
 os.system("aws sns subscribe --topic-arn %s --protocol email --notification-endpoint %s"%(arn,email))
 os.system("aws cloudwatch put-metric-alarm --alarm-name cpu-mon+%s --alarm-description 'Alarm when CPU exceeds 70 percent' --metric-name CPUUtilization --namespace AWS/EC2 --statistic Average --period 300 --threshold 70 --comparison-operator GreaterThanThreshold --dimensions  Name=InstanceId,Value=%s --evaluation-periods 2 --alarm-actions %s --unit Percent"%(ec2ID,ec2ID,arn))
